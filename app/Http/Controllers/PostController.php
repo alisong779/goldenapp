0b393c0710e8f5692fe12396a33d03e4bd87bc25
Alison Golden <alisong779@yahoo.com>
2025-06-21 13:15:37 -0400

    Add view post function and route, form validations and errors on create post , dynamic data in views

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,8 +23,13 @@ class PostController extends Controller
         $incomingFields['body'] = strip_tags($incomingFields['body']);
         $incomingFields['user_id'] = auth()->id();
 
-        Post::create($incomingFields);
+        $newPost = Post::create($incomingFields);
 
-        return redirect('/')->with('success', 'Post Added!');
+        return redirect("/post/{$newPost->id}")->with('success', 'New post successfully created.');
+    }
+
+    public function viewSinglePost(Post $post)
+    {
+        return view('/single-post', ['post' => $post]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ Route::post('/logout', [UserController::class, "logout"]);
 //Post related routes
 Route::get('/create-post', [PostController::class, "showCreateForm"]);
 Route::post('/create-post', [PostController::class, "storeNewPost"]);
+Route::get('/post/{post}', [PostController::class, "viewSinglePost"]);
